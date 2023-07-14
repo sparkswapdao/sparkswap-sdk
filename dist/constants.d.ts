4e903d4f4d6c7a41ea11647e8301ce91ddbd0d0e
empm0ney <99704063+empm0ney@users.noreply.github.com>
2023-07-14 12:16:37 -0400

    dist

---
+++ dist/constants.d.ts
@@ -0,0 +1,14 @@
+import { Pulse, Token } from '@pulsex/sdk-core';
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x1715a3e4a142d8b698131108995174f37aeba10d";
+export declare const WETH: {
+    369: Token;
+};
+export declare const ETHER: Pulse;
+export declare const INIT_CODE_HASH = "0x59fffffddd756cba9095128e53f3291a6ba38b21e3df744936e7289326555d62";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
