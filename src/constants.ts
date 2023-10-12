2089314a4e7fbb614829696dcb77b7ecd368e375
empm0ney <99704063+empm0ney@users.noreply.github.com>
2023-10-12 14:34:01 -0400

    new launch dex

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,13 @@
 import { Pulse, Token } from '@pulsex/sdk-core'
 import JSBI from 'jsbi'
 
-export const FACTORY_ADDRESS = '0xEE31dc699ff37940E657F2C07A7D331Eba54A6d2'
+export const FACTORY_ADDRESS = '0xf61f7C93387D1e99250D9Ebe9F3a26b53B164950'
 
 export const WETH = { 369: new Token(369, '0xa1077a294dde1b09bb078844df40758a5d0f9a27', 18, 'WPLS', 'Wrapped Pulse') }
 
 export const ETHER = { ...Pulse.onChain(369), wrapped: WETH[369], chainId: 369, equals: Pulse.onChain(369).equals } as Pulse
 
-export const INIT_CODE_HASH = '0x1d13db667562dd8d6e75796dcb1b654dd4aa9b64eb8152e2efae0fa4410fcc71'
+export const INIT_CODE_HASH = '0x0eeb74fefe5667c2eef945c113bf051629ddee029deaf9a88556dceb240d07d9'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
